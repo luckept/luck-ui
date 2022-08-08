fe1c0ef4f2cd7d2d1880549c50e2f07a01a98c77
luckept <luckept@qq.com>
2022-08-08 17:33:25 +0800

    chore: use prettier and eslint

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import Test from "./components/Test"
+import Test from './components/Test'
 </script>
 
 <template>
-  <Test></Test>
+  <Test />
 </template>
 
 <style scoped></style>
